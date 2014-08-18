c14ee0d86d7afe4eabc8b5ca0bef4ca537e1f6e7
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-08-18 23:27:35 +0800

    added points to map

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Hawaii::Application.routes.draw do
 
-  # scope :api, defaults: { format: :json } do
-  #   resources :businesses, except: [ :show, :new, :edit ] do
-  #     get ':id' => 'businesses#index', on: :collection
-  #   end
-  # end
+  scope :api, defaults: { format: :json } do
+    resources :businesses, only: [ :index ] #do
+    #   get ':id' => 'businesses#index', on: :collection
+    # end
+  end
 
   resources :businesses, only: [ :index, :create, :update ]
   get 'sample_data' => 'businesses#sample_data', as: :sample_data
