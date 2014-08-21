7ea79246320c5b8fb355e5e35ecbaef183e428da
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-08-21 12:48:39 +0800

    changed serve static assets and assets compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Hawaii::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
