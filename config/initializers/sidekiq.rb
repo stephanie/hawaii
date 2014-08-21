c1f3de90e9610a2f50bdb6a63898d2d1b9c21b81
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-08-21 14:41:27 +0800

    trying to configure redis for heroku production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,10 +5,14 @@ Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   username == 'stephs829' && password == '12345678'
 end
 
-# Sidekiq.configure_server do |config|
-#   config.redis = { :url => 'redis://redis.example.com:7372/12', :namespace => 'mynamespace' }
-# end
+if Rails.env.production?
 
-# Sidekiq.configure_client do |config|
-#   config.redis = { :url => 'redis://redis.example.com:7372/12', :namespace => 'mynamespace' }
-# end
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"]}
+  end
+  
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"]}
+  end
+
+end
